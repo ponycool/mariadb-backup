5833e03fd0930d0116f9f7ed0d9be2d85d8a884a
Pony <pony@ponycool.com>
2022-10-15 11:31:09 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+IMAGE=mariadb-backup
+VERSION=1.0
+
+#清除tag为none的镜像
+docker ps -a | grep "Exited" | awk '{print $1}' | xargs docker stop
+docker ps -a | grep "Exited" | awk '{print $2}' | xargs docker rm
+docker images | grep none | awk '{print $3}' | xargs docker rmi
+
+#清除已有的
+docker stop $IMAGE-$VERSION
+docker rm $IMAGE-$VERSION
+docker rmi ponycool/$IMAGE-$VERSION
+
+# 启用buildx插件，适用于v19.03+
+docker buildx create --use --name larger_log --node larger_log0 --driver-opt env.BUILDKIT_STEP_LOG_MAX_SIZE=10485760
+
+# 检查当前的构建器实例
+docker buildx inspect larger_log --bootstrap
+
+#重新生成
+docker buildx build -t ponycool/$IMAGE-$VERSION --load ./
